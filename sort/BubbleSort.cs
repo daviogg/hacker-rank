70327678c9de18560920fb3399f180ff4be479c9
Davide Oggioni (Reti) <daviogg@hotmail.it>
2021-06-01 14:32:30 +0200

    Push bubble sort

---
+++ sort/BubbleSort.cs
@@ -0,0 +1,9 @@
+for (int i = 0; i < n; i++) {
+    
+    for (int j = 0; j < n - 1; j++) {
+        // Swap adjacent elements if they are in decreasing order
+        if (a[j] > a[j + 1]) {
+            swap(a[j], a[j + 1]);
+        }
+    }
+}
